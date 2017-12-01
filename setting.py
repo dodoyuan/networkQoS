d63a2a6f390e23ab6fdbfcdc20d89ff95c907f92
dodoyuan <dodoyuan>
2017-12-01 20:53:46 +0800

    link capacity change

--- setting.py
+++ setting.py
@@ -13,7 +13,7 @@ MAX_CAPACITY = 281474976710655L  # Max capacity of link
 
 
 def get_link_capacity(dpid, port, return_matrix_flag):
-    link_capacity = defaultdict(lambda: defaultdict(lambda: 10))
+    link_capacity = defaultdict(lambda: defaultdict(lambda: 100))
     link_capacity[8][1] = 100
     link_capacity[8][2] = 100
     link_capacity[8][3] = 100
@@ -32,9 +32,9 @@ k_paths = 2
 WEIGHT = 'bw'
 
 # predefined requirement band-with of each source IP
-require_band = {"10.0.0.1": 8, "10.0.0.2": 8, "10.0.0.3": 8,
-                "10.0.0.4": 1, "10.0.0.5": 1, "10.0.0.6": 1}
+require_band = {"10.0.0.1": 40, "10.0.0.2": 80, "10.0.0.3": 50,
+                "10.0.0.4": 30, "10.0.0.5": 1, "10.0.0.6": 1}
 
 # predefined priority of each source IP
-priority_weight = {"10.0.0.1": 32, "10.0.0.2": 16, "10.0.0.3": 8,
-                   "10.0.0.4": 4,  "10.0.0.5": 2,  "10.0.0.6": 1}
+priority_weight = {"10.0.0.1": 16, "10.0.0.2": 8, "10.0.0.3": 4,
+                   "10.0.0.4": 2,  "10.0.0.5": 2,  "10.0.0.6": 1}
