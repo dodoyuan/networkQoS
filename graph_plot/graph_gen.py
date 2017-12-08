90c57fe73f5ad815eeb1501e4cddebd175e2b320
dodoyuan <dodoyuan>
2017-12-08 16:39:05 +0800

    graph gen

--- graph_plot/graph_gen.py
+++ graph_plot/graph_gen.py
@@ -11,9 +11,7 @@ from scipy.interpolate import spline
 
 def sender_plot():
 
-    x = [i for i in xrange(31)]
-    x_sm = np.array(x)
-    x_smooth = np.linspace(x_sm.min(), x_sm.max(), 200)
+    x = np.arange(0, 31, 1)
 
     y1 = all_data.sender_throughput.y1
     y2 = all_data.sender_throughput.y2
@@ -21,22 +19,17 @@ def sender_plot():
     y4 = all_data.sender_throughput.y4
 
     plt.figure(figsize=(15, 7))
+    plt.plot(x, y1, 'b', marker='+', label="high QoS level",
+             markersize=4, markevery=100)
 
-    y1_smooth = spline(x, y1, x_smooth)
-    plt.plot(x_smooth, y1_smooth, 'b', marker='+', label="high QoS level",
-             markersize=4)
+    plt.plot(x, y2, 'r', marker='*', label="medium QoS level",
+             markersize=4, markevery=100)
 
-    y2_smooth = spline(x, y2, x_smooth)
-    plt.plot(x_smooth, y2_smooth, 'r', marker='*', label="medium QoS level",
-             markersize=4)
+    plt.plot(x, y3, color='g', marker='x', markersize=4,
+             label="low QoS level", markevery=100)
 
-    y3_smooth = spline(x, y3, x_smooth)
-    plt.plot(x_smooth, y3_smooth, color='g', marker='x', markersize=4,
-             label="low QoS level")
-
-    y4_smooth = spline(x, y4, x_smooth)
-    plt.plot(x_smooth, y4_smooth, color='k', marker='o', markersize=4,
-             label="best effort")
+    plt.plot(x, y4, color='k', marker='o', markersize=4,
+             label="best effort", markevery=100)
 
     plt.ylabel('Throughput(Mbps)')
     plt.xlabel('Time(s)')
