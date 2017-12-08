38d807ebadc344a4aa316c50e3cf32c9a777672f
dodoyuan <dodoyuan>
2017-12-08 16:22:18 +0800

    graph gen

--- graph_plot/graph_gen.py
+++ graph_plot/graph_gen.py
@@ -13,7 +13,7 @@ def sender_plot():
     x = np.arange(0, 31, 1)
 
     x_sm = np.array(x)
-    x_smooth = np.linspace(x_sm.min(), x_sm.max(), 200)
+    x_smooth = np.linspace(x_sm.min(), x_sm.max(), 20)
 
     y1 = np.array(all_data.sender_throughput.y1)
     y2 = np.array(all_data.sender_throughput.y2)
@@ -27,15 +27,15 @@ def sender_plot():
              markersize=4)
 
     y2_smooth = spline(x, y2, x_smooth)
-    plt.plot(x, y2_smooth, 'r', marker='*', label="medium QoS level",
+    plt.plot(x_smooth, y2_smooth, 'r', marker='*', label="medium QoS level",
              markersize=4)
 
     y3_smooth = spline(x, y3, x_smooth)
-    plt.plot(x, y3_smooth, color='g', marker='x', markersize=4,
+    plt.plot(x_smooth, y3_smooth, color='g', marker='x', markersize=4,
              label="low QoS level")
 
     y4_smooth = spline(x, y4, x_smooth)
-    plt.plot(x, y4_smooth, color='k', marker='o', markersize=4,
+    plt.plot(x_smooth, y4_smooth, color='k', marker='o', markersize=4,
              label="best effort")
 
     plt.ylabel('Throughput(Mbps)')
