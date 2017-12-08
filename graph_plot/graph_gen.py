3176f02e3742689d0d18f8a18e29f8bed241950e
dodoyuan <dodoyuan>
2017-12-08 19:11:30 +0800

    graph gen

--- graph_plot/graph_gen.py
+++ graph_plot/graph_gen.py
@@ -69,7 +69,36 @@ def CSWP_plot():
     plt.legend(loc='best')
     plt.show()
 
+def ILP_plot():
+
+    x = np.arange(0, 31, 1)
+
+    y1 = all_data.ILP_throughput.y1
+    y2 = all_data.ILP_throughput.y2
+    y3 = all_data.ILP_throughput.y3
+    y4 = all_data.ILP_throughput.y4
+
+    plt.figure(figsize=(15, 7))
+    plt.plot(x, y1, 'b', marker='+', label="high QoS level",
+             markersize=8)
+
+    plt.plot(x, y2, 'r', marker='*', label="medium QoS level",
+             markersize=8)
+
+    plt.plot(x, y3, color='g', marker='x', markersize=8,
+             label="low QoS level")
+
+    plt.plot(x, y4, color='k', marker='o', markersize=8,
+             label="best effort")
+
+    plt.ylabel('Throughput(Mbps)')
+    plt.xlabel('Time(s)')
+    plt.yticks(np.arange(0, 10, 1))
+    plt.legend(loc='best')
+    plt.show()
+
 if __name__ == '__main__':
     sender_plot()
     CSWP_plot()
+    ILP_plot()
 
