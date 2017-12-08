41826c3b0efe75f16b2f10164798beec92867820
dodoyuan <dodoyuan>
2017-12-08 14:55:33 +0800

    graph gen

---
+++ graph_plot/graph_gen.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+# --*-- coding:utf8--*--
+
+from collections import defaultdict
+#from shortestSum import mod_dijkstra
+import numpy as np
+import matplotlib.pyplot as plt
+import all_data
+
+
+def sender_plot():
+    x = np.arange(1, 61, 1)
+    y1 = all_data.sender_throughput.y1
+    y2 = all_data.sender_throughput.y2
+    y3 = all_data.sender_throughput.y3
+    y4 = all_data.sender_throughput.y4
+
+    plt.figure(figsize=(15, 7))
+    plt.plot(x, y1, 'b--', marker='+', label="$high QoS level$")
+    plt.plot(x, y2, 'r--', marker='*', label="$medium QoS level$")
+    plt.plot(x, y3, color='g',linestyle='--', marker='x',
+             label="$low QoS level$",
+             linewidth=2)
+    plt.plot(x, y4, color='k', linestyle='--', marker='2',
+             label="$best effort$",
+             linewidth=2)
+    plt.ylabel('average controller plane availability')
+    plt.xlabel('Node availability')
+    plt.legend(loc='best')
+    plt.show()
+
