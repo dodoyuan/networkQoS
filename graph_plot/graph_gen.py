a0b6cbcaecde386a3229aa3f9d1a45d3522839a8
dodoyuan <dodoyuan>
2017-12-08 20:31:12 +0800

    graph gen

--- graph_plot/graph_gen.py
+++ graph_plot/graph_gen.py
@@ -79,16 +79,16 @@ def ILP_plot():
     y4 = all_data.ILP_throughput.y4
 
     plt.figure(figsize=(10, 7))
-    plt.plot(x, y1, 'b', marker='+', label="high QoS level",
+    plt.plot(x, y1, 'k', marker='+', label="high QoS level",
              markersize=12)
 
-    plt.plot(x, y2, 'r', marker='*', label="medium QoS level",
+    plt.plot(x, y2, 'r', marker='s', label="medium QoS level",
              markersize=12)
 
-    plt.plot(x, y3, color='g', marker='x', markersize=12,
+    plt.plot(x, y3, color='g', marker='^', markersize=12,
              label="low QoS level")
 
-    plt.plot(x, y4, color='k', marker='o', markersize=12,
+    plt.plot(x, y4, color='b', marker='o', markersize=12,
             label="best effort")
 
     plt.ylabel('Throughput(Mbps)')
