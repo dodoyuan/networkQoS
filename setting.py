3ccdc31159fedeac7c1867f6848dc776c936c4a9
dodoyuan <dodoyuan>
2017-12-21 13:25:12 +0800

    graph gen

--- setting.py
+++ setting.py
@@ -32,7 +32,7 @@ k_paths = 2
 WEIGHT = 'bw'
 
 # predefined requirement band-with of each source IP
-require_band = {"10.0.0.1": 8, "10.0.0.2": 6, "10.0.0.3": 5}
+require_band = {"10.0.0.1": 8, "10.0.0.2": 4, "10.0.0.3": 5}
 
 # predefined priority of each source IP
 priority_weight = {"10.0.0.1": 16, "10.0.0.2": 8, "10.0.0.3": 4}
